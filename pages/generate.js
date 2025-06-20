989eb00c26fda45af95a1cd39a29a76be0217b98
SaadM59 <saad.mejdoubi14@gmail.com>
2025-06-20 15:59:24 +0200

    Update and rename generate.jsx to generate.js

--- pages/generate.js
+++ pages/generate.js
@@ -1,6 +1,5 @@
 import dynamic from 'next/dynamic'
 
-// Charge le composant Generate en client-only
 const GenerateClient = dynamic(
   () => import('../app/generate/page-client'),
   { ssr: false }
