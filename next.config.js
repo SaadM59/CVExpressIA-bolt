31d4e92d1177293974fd31493552153db99463bd
SaadM59 <saad.mejdoubi14@gmail.com>
2025-06-20 14:47:55 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   // on passe à l’export 100 % statique
   output: 'export',
+  trailingSlash: true,
   // on ignore les erreurs TypeScript pour qu’il build toujours
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
